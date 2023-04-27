b9cf7ec0d6f43ff198b5a20b9549e3040ceec88b
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2023-04-27 19:36:13 +0100

    finished type casting lesson

---
+++ lessons/lesson_five/main.py
@@ -0,0 +1,18 @@
+# This is typecasting
+
+x = 1  # Int
+y = 2.0  # float
+z = "3"  # string
+
+# Permeability resigns it
+
+x = float(x)
+y = str(y)
+
+# String is not a number so cant numerical expressions
+z = int(z)
+
+print(y)
+print(z*3)
+print(x)
+
