428ea34414e5ae9298eadb1f0da3bc8ee72cf233
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2023-04-27 19:18:28 +0100

    finished Multiple assignment

---
+++ lessons/lesson_three/main.py
@@ -0,0 +1,19 @@
+# Multiple assignment
+
+# name = "Yusuf"
+# age = 16
+# knows_how_to_code = True
+
+# This can be done like
+
+name, age, knows_how_to_code = "Yusuf", 16, True
+
+print("My name is " + name + " and I am " + str(age) + " years old and I know how to code " + str(knows_how_to_code))
+
+# bob_age = 20
+# kane_age = 20
+# messi_age = 20
+
+# This can just be done like
+
+bob_age = kane_age = messi_age = 20
