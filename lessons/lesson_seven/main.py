8c89fba0c6c9fe032919994703a3b49eedf48a45
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2023-04-27 19:56:18 +0100

    Finished with  Math useful methods

---
+++ lessons/lesson_seven/main.py
@@ -0,0 +1,32 @@
+# Math useful methods
+
+import math
+
+pi = 3.145
+x = 1
+y = 2
+z = 3
+
+# Rounds the number
+# print(round(pi))
+
+# Rounds the number up
+# print(math.ceil(pi))
+
+# Rounds the number down
+# print(math.floor(pi))
+
+# Gets the absolute value of a number (how far it from 0)
+# print(abs(pi))
+
+# Gives a number to its power ie if 2 is passed it will square it.
+# print(pow(pi, 2))
+
+# Square roots a number
+# print(math.sqrt(pi))
+
+# Find the largest value from the given values.
+# print(max(x, y, z))
+
+# Finds the lowest value from the given values
+# print(min(x, y, z))
