1261dfe49228363a4e87ff5df9d75cd5a4d331b4
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2023-04-27 19:27:33 +0100

    finished with useful method lesson

---
+++ lessons/lesson_four/main.py
@@ -0,0 +1,33 @@
+# Useful string methods
+
+name = "Yusuf"
+uncaptlised_name = "yusuf"
+
+# print(len(name))
+
+# Computers start at 0
+# print(name.find("Y"))
+
+# Only first litter
+# print(uncaptlised_name.capitalize())
+
+# Make all letters capital
+# print(name.upper())
+
+# Make all the letter un capitalised.
+# print(name.lower())
+
+# Check if the string is a digit (numer)
+# print(name.isdigit())
+
+# Whether the string contains letter. Will be false if there is a space as space is a character.
+# print(name.isalpha())
+
+# How many characters there is in our string for provided letter
+# print(name.count("Y"))
+
+# Replaces characters
+# print(name.replace("u", "eu"))
+
+# Used to display the string a set amount of times
+# print(name * 5)
