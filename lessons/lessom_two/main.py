f44b37aee20e4b02bc6793d3d78fc7ec1be94057
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2023-04-27 19:13:58 +0100

    Finished vairable lesson

---
+++ lessons/lessom_two/main.py
@@ -0,0 +1,29 @@
+# This is a variable lesson
+
+first_name = "Yusuf"
+last_name = "Ismail"
+full_name = first_name + " " + last_name
+print("My full name is " + full_name)
+
+# Check the data type
+# str is lot toString
+print("This is the data type " + str(type(full_name)))
+
+# Use appropriate data type
+age = 16
+age += 1
+print("My age is " + str(age))
+print("This is the data type for age " + str(age))
+
+# Decimal = float
+height = 250.5
+print("My height is " + str(height) + "cm")
+print("This is the data type for height " + str(height))
+
+# Boolean true or false. First letter has to be in caps
+
+my_name_is_yusuf = True
+print("My name is Yusuf " + str(my_name_is_yusuf))
+print("This is the data type for question " + str(my_name_is_yusuf))
+
+
